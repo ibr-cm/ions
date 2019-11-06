7269b167c15e823c13b7bf63c975b3480b06d2f9
Christian Hagau <hagau@ibr.cs.tu-bs.de>
2019-11-06 00:00:00 +0000

    Add variable-dependant y-axis labeling and range

--- plots.py
+++ plots.py
@@ -17,9 +17,10 @@ class Plot:
 
 
 class SimplePlot(Plot):
-    def __init__(self, fltr:Filter, x_axis):
+    def __init__(self, fltr:Filter, x_axis, y_axis):
         Plot.__init__(self, fltr)
         self.x_axis = x_axis
+        self.y_axis = y_axis
 
     def plot(self, ax, df, x_row, label):
         print("Implement this")
@@ -29,10 +30,12 @@ class SimplePlot(Plot):
         ticklabel = ['0', '2', '4', '6', '8', '10', '12', '14', '16', '18', '20', '22']
         return ticks, ticklabel
 
+    #TODO: hacky
     def get_ticks_mp(self):
         ticks = [ 5, 10, 25, 50, 75, 100 ]
         ticks_norm = [ x/100 for x in ticks ]
         ticklabel = [ str(x) for x in ticks]
+        ticks = ticks_norm
         return ticks, ticklabel
 
 
@@ -56,6 +59,7 @@ class SimplePlot(Plot):
         # print("x_axis: ", self.x_axis,"  df: ", df)
 
 
+        # TODO: this is probably not great for boxplots
         for df in dfs:
             # label = df_sel['v2x_rate'].head(n=1)
             # TODO: robust label propagation
@@ -65,17 +69,14 @@ class SimplePlot(Plot):
             self.plot(ax, df, self.x_axis, label=label)
 
         # TODO:
-        # ax.set_xlabel('Market Penetration Rate in %', fontsize=FONTSIZE_LABEL)
-        # ax.set_ylabel('Average '+map_var_name(y_axis), fontsize=FONTSIZE_LABEL)
-        ax.set_ylabel('Average '+"CBR", fontsize=FONTSIZE_LABEL)
+        ax.set_ylabel('Average '+map_var_name(self.y_axis), fontsize=FONTSIZE_LABEL)
         ax.set_xlabel(map_xlabel_name(self.x_axis), fontsize=FONTSIZE_LABEL)
 
         # TODO:
         # ax.set_xmargin(0.01)
-        ax.set_ylim((0, 0.8))
         # ax.set_xmargin(1.01)
         # TODO: 
-        # ax.set_ylim(map(self.y_axis))
+        ax.set_ylim(map_yrange_name(self.y_axis))
 
         # TODO:
         ticks, ticklabel = self.get_ticks(self.x_axis)
@@ -100,9 +101,10 @@ class SimplePlot(Plot):
 #-----------------------------------------------------------------------------
 
 class BoxPlot(SimplePlot):
-    def __init__(self, fltr:Filter, x_axis):
+    def __init__(self, fltr:Filter, x_axis, y_axis):
         Plot.__init__(self, fltr)
         self.x_axis = x_axis
+        self.y_axis = y_axis
 
     def plot(self, ax, df_sel, x_row, label):
         # BOXPLOT
@@ -133,7 +135,10 @@ class BoxPlot(SimplePlot):
 
             # TODO: hacky
             style = b['gen_rule']
-            delta = position/100.0 * 0.1
+            if ',' in style:
+                style = style.split(',')[0]
+            # delta = position/100.0 * 0.1
+            delta = 0.2
             if style=='static':
                 position -= delta
             else:
@@ -144,11 +149,13 @@ class BoxPlot(SimplePlot):
             print("key: ", key)
             print("label: ", label)
             print("position: ", position)
+            print("style: ", style)
             print("--------------------------")
 
             # plot = ax.bxp(b['bxp'].values, positions=[b[x_row]], boxprops=boxprops, patch_artist=True)
             # plot = ax.bxp([val], boxprops=boxprops, patch_artist=True)
-            # set_boxplot_style(plot, style)
+            plot = ax.bxp([val], positions=[position], boxprops=boxprops, patch_artist=True)
+            set_boxplot_style(plot, style)
 
             # offset += 0.1 * key
             # if offset > 30:
@@ -159,15 +166,17 @@ class BoxPlot(SimplePlot):
         # plot = ax.bxp(b['bxp'].values, positions=[b[x_row]], widths=64.0, boxprops=boxprops, patch_artist=True)
         # plot = ax.bxp(b['bxp'].values, positions=[b[x_row]], boxprops=boxprops, patch_artist=True)
 
-        plot = ax.bxp(bxps, positions=positions, boxprops=boxprops, patch_artist=True, widths=1.5)
+        # plot = ax.bxp(bxps, positions=positions, boxprops=boxprops, patch_artist=True, widths=1.5)
+        # plot = ax.bxp(bxps, positions=positions, boxprops=boxprops, patch_artist=True)
         # plot = ax.bxp(bxps, boxprops=boxprops, patch_artist=True)
-        set_boxplot_style(plot, style)
+        # set_boxplot_style(plot, style)
 
 
 class LinePlot(SimplePlot):
-    def __init__(self, fltr:Filter, x_axis):
+    def __init__(self, fltr:Filter, x_axis, y_axis):
         Plot.__init__(self, fltr)
         self.x_axis = x_axis
+        self.y_axis = y_axis
 
     def plot(self, ax, df_sel, x_row, label):
         # LINEPLOT
