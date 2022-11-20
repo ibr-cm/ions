6ee64eb85f3e955ee37cdd4c09eb6b00394db73e
Christian Hagau <hagau@ibr.cs.tu-bs.de>
2022-11-20 00:00:00 +0000

    Allow running only a subset of the tasks of a recipe

--- run_recipe.py
+++ run_recipe.py
@@ -46,6 +46,9 @@ def execute_evaluation_phase(recipe:Recipe, options):
 
     data_repo = {}
     for extractor_name in recipe.evaluation.extractors:
+        if options.run_tree and not extractor_name in options.run_tree['evaluation']['extractors']:
+            print(f'skipping extractor {extractor_name}')
+            continue
         extractor = recipe.evaluation.extractors[extractor_name]
 
         if extractor_name in options.extraction_overrides:
@@ -58,17 +61,31 @@ def execute_evaluation_phase(recipe:Recipe, options):
         # print(f'-<-<-<-<-<-<-')
         data_repo[extractor_name] = delayed_data
 
+        print(f'added extractor {extractor_name}')
+
     if not hasattr(recipe.evaluation, 'transforms'):
         print('execute_evaluation_phase: no `transforms` in recipe.Evaluation')
         return data_repo
 
     for transform_name in recipe.evaluation.transforms:
+        if options.run_tree and not transform_name in options.run_tree['evaluation']['transforms']:
+            print(f'skipping transform {transform_name}')
+            continue
         transform = recipe.evaluation.transforms[transform_name]
         transform.set_data_repo(data_repo)
         transform.execute()
+        print(f'added transform {transform_name}')
 
     jobs = []
+
+    if recipe.evaluation.exporter is None:
+        print('execute_evaluation_phase: no `exporter` in recipe.Evaluation')
+        return data_repo
+
     for exporter_name in recipe.evaluation.exporter:
+        if options.run_tree and not exporter_name in options.run_tree['evaluation']['exporter']:
+            print(f'skipping exporter {exporter_name}')
+            continue
         exporter = recipe.evaluation.exporter[exporter_name]
 
         if exporter_name in options.export_overrides:
@@ -77,7 +94,8 @@ def execute_evaluation_phase(recipe:Recipe, options):
 
         exporter.set_data_repo(data_repo)
         job = exporter.execute()
-        jobs.append(job)
+        jobs.extend(job)
+        print(f'added exporter {exporter_name}')
 
 
     print(f'{jobs=}')
@@ -98,6 +116,9 @@ def execute_plotting_phase(recipe:Recipe, options):
     data_repo = {}
 
     for dataset_name in recipe.plot.reader:
+        if options.run_tree and not dataset_name in options.run_tree['plot']['reader']:
+            print(f'skipping exporter {dataset_name}')
+            continue
         reader = recipe.plot.reader[dataset_name]
         print(f'plot: loading dataset: "{dataset_name=}"')
         if dataset_name in options.reader_overrides:
@@ -105,18 +126,26 @@ def execute_plotting_phase(recipe:Recipe, options):
             print(f'plot: execute_plotting_phase overriding input files for "{dataset_name}": "{reader.input_files=}"')
         data = reader.read_data()
         data_repo[dataset_name] = data
+        print(f'added reader {dataset_name}')
 
     print('<<<-<-<--<-<-<--<-<-<')
     print(f'plot: {data_repo=}')
     print('<<<-<-<--<-<-<--<-<-<')
 
     for task_name in recipe.plot.transforms:
+        if options.run_tree and not task_name in options.run_tree['plot']['transforms']:
+            print(f'skipping transform {task_name}')
+            continue
         task = recipe.plot.transforms[task_name]
         task.set_data_repo(data_repo)
         task.execute()
+        print(f'added transform {task_name}')
 
     jobs = []
     for task_name in recipe.plot.tasks:
+        if options.run_tree and not task_name in options.run_tree['plot']['tasks']:
+            print(f'skipping task {task_name}')
+            continue
         task = recipe.plot.tasks[task_name]
         print(f'plot: {task_name=}')
         print(f'plot: {task=}')
@@ -133,6 +162,7 @@ def execute_plotting_phase(recipe:Recipe, options):
         job = task.execute()
         # print(f'plot: {job=}')
         jobs.append(job)
+        print(f'added task {task_name}')
 
     if options.plot_task_graphs:
         for i in range(0, len(jobs)):
@@ -187,6 +217,7 @@ def parse_args():
 
     parser.add_argument('--eval-only', action='store_true', default=False, help='run eval phase only')
     parser.add_argument('--plot-only', action='store_true', default=False, help='run plot phase only')
+    parser.add_argument('--run', type=str, default='all', help='run selected tasks/steps only') #TODO:description
 
     parser.add_argument('--worker', type=int, default=4, help='the number of worker processes')
 
@@ -219,6 +250,45 @@ def parse_args():
 
     set_dict_arg_from_string(args.override_reader, 'reader_overrides')
     set_dict_arg_from_string(args.override_plot, 'plot_overrides')
+
+
+    def get_run_tree(option, phase_names):
+        d = dict()
+        d[phase_names[0]] = set()
+        d[phase_names[1]] = set()
+        d[phase_names[2]] = set()
+        for qualified_task_name in option.split(','):
+            if len(qualified_task_name) == 0:
+                continue
+            top_level, task_name = qualified_task_name.split('.')
+            d[top_level] = d[top_level].union(set([task_name]))
+        return d
+
+    def process_run_tree(option):
+        eval_phases = ['extractors', 'transforms', 'exporter']
+        plot_phases = ['reader', 'transforms', 'tasks']
+
+        p = option.split(':')
+        if len(p) == 1 or (len(p) == 2 and len(p[1]) == 0):
+            # evaluation phase only
+            eval_str = p[0]
+            plot_str = ''
+        else:
+            eval_str = p[0]
+            plot_str = p[1]
+
+        run_tree = { 'evaluation': get_run_tree(eval_str, eval_phases)
+                    , 'plot': get_run_tree(plot_str, plot_phases)
+                    }
+        return run_tree
+
+    if args.run != 'all':
+        run_tree = process_run_tree(args.run)
+    else:
+        run_tree = None
+
+    setattr(args, 'run_tree', run_tree)
+    print(f'{run_tree=}')
 
     return args
 
