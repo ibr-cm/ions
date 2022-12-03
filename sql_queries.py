f6814b26c861d57a6b6e018572c04a6b49fe3697
Christian Hagau <hagau@ibr.cs.tu-bs.de>
2022-12-03 00:00:00 +0000

    Add a SQLAlchemy query generator for retrieving signal values with the associated position

--- sql_queries.py
+++ sql_queries.py
@@ -2,6 +2,7 @@ import sqlalchemy as sqla
 
 from sql_model import OmnetppTableModel as TM
 
+from common.logging_facilities import logi
 
 # """
 # SELECT attrName, attrValue
@@ -65,5 +66,121 @@ def generate_data_query(where_clause:sqla.sql.elements.ColumnElement
                        .where(
                               where_clause
                              )
+
+    return query
+
+
+def get_signal_with_position(x_signal:str, y_signal:str
+                  , value_label_px:str, value_label_py:str
+                  , signal_name:str, value_label:str
+                  , restriction:tuple=None
+                  , moduleName:bool=True
+                  , simtimeRaw:bool=True
+                  , eventNumber:bool=False
+                  ):
+    '''
+    The equivalent SQL query:
+
+    WITH pxvids
+    AS (SELECT vectorId, moduleName FROM vector AS v WHERE vectorName == '<positionX>'),
+    pyvids
+    AS (SELECT vectorId, moduleName FROM vector AS v WHERE vectorName == '<positionY>'),
+    pys AS
+        (SELECT moduleName, eventNumber, simtimeRaw, value AS posY
+         FROM
+            pyvids
+         JOIN vectorData AS vd
+            ON vd.vectorId == pyvids.vectorId
+        WHERE vd.value < <y_max> AND vd.value > <y_min>
+        ),
+    pxs AS
+        (SELECT moduleName, eventNumber, simtimeRaw, value AS posX
+         FROM
+            pxvids
+         JOIN vectorData AS vd
+            ON vd.vectorId == pxvids.vectorId
+        WHERE vd.value < <x_max> AND vd.value > <x_min>
+         ),
+    pos AS
+        (SELECT pxs.moduleName, pxs.eventNumber, pxs.simtimeRaw, posX, posY
+         FROM
+             pxs
+         JOIN
+             pys
+             ON pxs.eventNumber == pys.eventNumber
+        ),
+    val AS
+        (SELECT vd.vectorId, vd.eventNumber, vd.value
+         FROM vector AS v
+         JOIN vectorData AS vd
+            ON v.vectorId == vd.vectorId
+         WHERE v.vectorName == '<signal_name>'
+        )
+    SELECT p.moduleName, p.eventNumber, p.simtimeRaw, posX, posY, v.value
+    FROM pos AS p
+    JOIN val AS v
+        ON p.vectorId == v.vectorId
+            AND p.eventNumber == v.eventNumber
+        ;
+    '''
+
+    # get the vectorIds for the x & y position signals
+    pxidsq = sqla.select(TM.vector_table.c.vectorId, TM.vector_table.c.moduleName) \
+              .where(TM.vector_table.c.vectorName == x_signal)
+    pyidsq = sqla.select(TM.vector_table.c.vectorId, TM.vector_table.c.moduleName) \
+              .where(TM.vector_table.c.vectorName == y_signal)
+
+    # get the data for the x & y position vectorIds, each within a given interval
+    pxs = sqla.select(pxidsq.c.moduleName
+                      , TM.vectorData_table.c.eventNumber
+                      , TM.vectorData_table.c.simtimeRaw
+                      , TM.vectorData_table.c.value
+                      ).join(TM.vectorData_table
+                             , TM.vectorData_table.c.vectorId == pxidsq.c.vectorId)
+
+    pys = sqla.select(pyidsq.c.moduleName
+                      , TM.vectorData_table.c.eventNumber
+                      , TM.vectorData_table.c.simtimeRaw
+                      , TM.vectorData_table.c.value
+                      ).join(TM.vectorData_table
+                             , TM.vectorData_table.c.vectorId == pyidsq.c.vectorId)
+
+    # apply geographic restriction: check if the position is within a rectangular area
+    if restriction:
+        logi(f'applying geographic restriction with parameters: {restriction=}')
+        x_min, y_min, x_max, y_max = restriction
+        pxs = pxs.where(TM.vectorData_table.c.value <= x_max).where(TM.vectorData_table.c.value >= x_min)
+        pys = pys.where(TM.vectorData_table.c.value <= y_max).where(TM.vectorData_table.c.value >= y_min)
+
+    # join x & y positions over the eventNumber
+    position_join = sqla.join(pxs, pys, pxs.c.eventNumber == pys.c.eventNumber)
+
+    # select the proper columns
+    positions = sqla.select(pxs.c.moduleName, pxs.c.eventNumber, pxs.c.simtimeRaw
+                    , pxs.c.value.label('px'), pys.c.value.label('py')
+                    ).select_from(position_join)
+
+    # get the actual signal values
+    vals = sqla.select(TM.vector_table.c.vectorId, TM.vectorData_table.c.eventNumber, TM.vectorData_table.c.value) \
+              .where(TM.vector_table.c.vectorName == signal_name \
+                      ).join(
+                              TM.vectorData_table
+                              , TM.vectorData_table.c.vectorId == TM.vector_table.c.vectorId
+                              )
+
+    columns = []
+    if moduleName:
+        columns.append(positions.c.moduleName)
+    if simtimeRaw:
+        columns.append(positions.c.simtimeRaw)
+    if eventNumber:
+        columns.append(positions.c.eventNumber)
+
+    # join positions with signal values
+    position_vals_join = sqla.join(positions, vals, positions.c.eventNumber == vals.c.eventNumber)
+    query = sqla.select(*columns
+                    , positions.c.px.label(value_label_px), positions.c.py.label(value_label_py)
+                    , vals.c.value.label(value_label)
+                    ).select_from(position_vals_join)
 
     return query
