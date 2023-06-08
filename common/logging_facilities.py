809664543a50a8c4dd10876fb8d314f9acb4e9f3
Christian Hagau <hagau@ibr.cs.tu-bs.de>
2023-06-08 00:00:00 +0000

    Silence ipython parsing logging

--- common/logging_facilities.py
+++ common/logging_facilities.py
@@ -7,10 +7,13 @@ from logging import error as loge
 
 # ---
 
-def setup_logging_defaults(level=logging.WARNING, silence_font_manager:bool=True):
+def setup_logging_defaults(level=logging.WARNING, silence_font_manager:bool=True, silence_ipython_parser:bool=True):
     logging.basicConfig(format='%(levelname)s | %(module)s::%(name)s::%(funcName)s | %(message)s', level=level)
     if silence_font_manager:
         logging.getLogger('matplotlib.font_manager').setLevel(logging.WARNING)
+    if silence_ipython_parser:
+        logging.getLogger('parso.python.diff').setLevel(logging.WARNING)
+        logging.getLogger('parso.cache').setLevel(logging.WARNING)
 
 def set_logging_level(level=logging.WARNING):
     logging.getLogger().setLevel(level)
