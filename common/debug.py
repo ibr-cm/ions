4b329d4ecb4ec0f77609c99ac856ce20ab8c90e1
Christian Hagau <hagau@ibr.cs.tu-bs.de>
2024-06-02 19:02:47 +0200

    common: add a helper function for dropping into an ipdb interactive debugger session.
    This improves over the IPython.start_ipython shell by providing stack
    information, stack navigation and all the other functionality provided
    by pdb/ipdb/IPython.core.debugger.

--- common/debug.py
+++ common/debug.py
@@ -10,3 +10,21 @@ def start_ipython_dbg_cmdline(user_ns=None):
         start_ipython(argv=[], user_ns=user_ns)
     else:
         start_ipython(argv=[])
+
+def start_debug(frame=None):
+    r"""
+    Start an interactive ipdb debugging session in the given frame context.
+    If no frame is provided, use the caller's frame.
+    For information on frame objects see `inspect <https://docs.python.org/3/library/inspect.html>`_.
+
+    Parameters
+    ----------
+    frame: Optional[frame]
+        The frame context of interest.
+    """
+    import ipdb
+    if not frame:
+        # get the frame of the caller
+        import inspect
+        frame = inspect.currentframe().f_back
+    ipdb.set_trace(frame=frame)
